31b58f3dc0310b86d383028115e66885ca238182
Gaurav Sharma <gauravsharmaa723@gmail.com>
2026-05-21 16:35:58 +0530

    Practiced JavaScript basics

---
+++ variables.js
@@ -0,0 +1,9 @@
+//Store your name, age, and city in variables and print them.
+
+const name= "Gaurav Sharma";
+const age=33;
+const city="Ghaziabad";
+
+console.log("Name: " + name);
+console.log("Age: " + age);
+console.log("City: " + city);
