7f57a98c0ccb2c13f8b6784c0551fcdd19a24f4c
Gaurav Sharma <gauravsharmaa723@gmail.com>
2026-05-22 11:16:49 +0530

    Practiced conditions in JavaScript.

--- conditions.js
+++ conditions.js
@@ -6,3 +6,27 @@ if(num%2===0){
     console.log("number is even");
 
 }
+
+
+// Age Checker: Check if a person is eligible to vote (age 18 or above).
+
+const age=16;
+if(age>=18){
+    console.log("You are eligible to vote.");
+}
+else{
+    console.log("You are not eligible to vote.");
+}
+
+//largest of two numbers
+
+const num1=19;
+const num2=24;
+
+if(num1>num2){
+    console.log(`${num1} is larger than ${num2}`);
+}else if(num2>num1){
+    console.log(`${num2} is larger than ${num1}`);
+}else{
+    console.log(`${num1} and ${num2} are equal.`);
+}
--- variables.js
+++ variables.js
@@ -3,7 +3,15 @@
 const name= "Gaurav Sharma";
 const age=33;
 const city="Ghaziabad";
+const favoriteLanguage="JavaScript";
+const yearsOfExperience=5;
+const currentlyLearning="React";
 
-console.log("Name: " + name);
-console.log("Age: " + age);
-console.log("City: " + city);
+console.log(`
+    My name is ${name}, 
+    I am ${age} years old and I live in ${city}. 
+    My favorite language is ${favoriteLanguage} and 
+    I have ${yearsOfExperience} years of experience. 
+    Currently, I am learning ${currentlyLearning}.`);
+console.log(typeof name);
+console.log(typeof age);
